bdff731ad58b6f9ebfd40e585f9469cd038f1c04
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-06-14 01:05:38 -0300

    Ends shinyapp

--- 2-visualizations/shinyapp/server.R
+++ 2-visualizations/shinyapp/server.R
@@ -9,28 +9,28 @@ data <- read_csv("series_from_imdb.csv")
 
 shinyServer(function(input, output) {
   
-  dataInput <- reactive({
-    filter(data, series_name %in% c(input$select_series))
-  })
-  
   finalInput <- reactive({
-    filter(dataInput(), season <= input$select_seasons[1])
+    filter(data, series_name %in% c(input$select_series)) %>%
+    filter(season <= input$select_seasons[1])
   })
   
   output$lineplot <- renderPlotly({
     
-    plot_ly(data=finalInput(), x=~as.numeric(series_ep)) %>%
-    add_trace(y=~UserRating, color=~series_name, name="Nota", type='scatter', mode='lines') %>%
+    plot_ly(data=finalInput(), x=~as.numeric(series_ep), y=~UserRating, color=~series_name) %>%
+    add_trace(type='scatter', mode='lines') %>%
+    add_trace(size=~log(UserVotes/sqrt(UserVotes)), type='scatter', alpha=0.9) %>%
       
-    layout(yaxis = list(title="Avaliação geral do episódio"),
+    layout(title = "Notas gerais de episódios",
+           yaxis = list(title="Avaliação geral do episódio"),
            xaxis = list(title="Número do episódio na série"))
   })
   
   output$scatter <- renderPlotly({
     plot_ly(data=finalInput(), x=~as.numeric(series_ep), size=~log(UserVotes/sqrt(UserVotes))) %>%
-    add_trace(y=~UserVotes, color=~series_name, name="Nota", type='scatter', alpha=0.9) %>%
+    add_trace(y=~UserVotes, color=~series_name, type='scatter', alpha=0.9) %>%
     
-    layout(yaxis = list(title="Número de votos do episódio"),
+    layout(title = "Número de votos de episódios",
+           yaxis = list(title="Número de votos do episódio"),
            xaxis = list(title="Número do episódio na série"))
   })
   
--- 2-visualizations/shinyapp/ui.R
+++ 2-visualizations/shinyapp/ui.R
@@ -4,39 +4,40 @@ library(tidyr)
 library(readr)
 library(plotly)
 
+dados <- read_csv("series_from_imdb.csv")
+
 shinyUI(fluidPage(
-  titlePanel(h1("Popularidade de uma série com base em nas avaliações e votos de seus episódios", align = "center")),
+  titlePanel(h1("Popularidade de uma série com base nas avaliações e nos votos de seus episódios", align = "center")),
   
-  column(3,
-         h3("TIPS:"),
-         helpText("Selecione as séries de que deseja saber informações.",
-                  "A partir do a avaliação do episódio e da quantidade de votos que recebeu,",
-                  "veja quão popular é a série!",
-                  "Filtre pelo número da temporada também! :)"),
-         
-         selectInput("select_series", label = h3("Séries"),
-                     choices = unique(data$series_name), multiple=T),
-         
-         sliderInput("select_seasons", label = h3("Temporadas"),
-                     min=1, max=max(data$season), value=6),
-         
-         helpText("OBS: o diâmetro do círculo é proporcional ao número de votos do episódios que o círculo representa.")
-  ),
+  br(),
+  br(),
   
-  mainPanel(
-    fluidRow(
-      box(
-        plotlyOutput("lineplot", width = "100%"),
-        title = "Notas gerais de episódios",
-        width = 12
-      ),
-      
-      box(
-        plotlyOutput("scatter", width = "100%"),
-        title = "Número de votos de episódios",
-        width = 12
-      )
+  sidebarLayout(
+                sidebarPanel(
+                  h5("TIPS:"),
+                  helpText("Selecione as séries de que deseja saber informações.",
+                           "A partir da avaliação do episódio e da quantidade de votos que este recebeu,",
+                           "veja quão popular é a série!",
+                           "Filtre pelo número da temporada também! :)",
+                           "OBS: o diâmetro do círculo é proporcional ao número de votos do episódio que o círculo representa."),
+                  
+                  selectInput("select_series", label = h3("Séries"),
+                              choices = unique(dados$series_name), multiple=T),
+                  
+                  sliderInput("select_seasons", label = h3("Temporadas"),
+                              min=1, max=max(dados$season), value=6)
+                ),
+                
+                mainPanel(
+                  fluidRow(
+                      plotlyOutput("lineplot", width = "90%"),
+                      
+                      br(),
+                      br(),
+                      
+                      plotlyOutput("scatter", width = "90%")
+                  )
+                )
     )
-  )
 ))
 
